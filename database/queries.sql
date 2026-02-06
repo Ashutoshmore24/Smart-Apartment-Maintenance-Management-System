3ff346e7ef0b8a3ba80e7f53cb34156e0a9439c5
Ashutosh Sanjay More <ashutosh.more24@pccoepune.org>
2026-02-07 00:12:02 +0530

    Revised Folder Structure Design

--- database/queries.sql
+++ database/queries.sql
@@ -32,3 +32,27 @@ SELECT request_id, request_type, status
 FROM maintenance_request
 WHERE status != 'Completed';
 
+SELECT name
+FROM resident
+WHERE resident_id IN (
+    SELECT resident_id
+    FROM maintenance_request
+    WHERE status = 'Pending'
+);
+
+SELECT f.flat_id, COUNT(*) AS total_requests
+FROM maintenance_request mr
+JOIN resident r ON mr.resident_id = r.resident_id
+JOIN flat f ON r.flat_id = f.flat_id
+GROUP BY f.flat_id
+HAVING COUNT(*) > 1;
+
+
+SELECT a.apartment_name,
+       SUM(mb.base_charge + mb.penalty_amount) AS total_revenue
+FROM apartment a
+JOIN block b ON a.apartment_id = b.apartment_id
+JOIN flat f ON b.block_id = f.block_id
+JOIN maintenance_bill mb ON f.flat_id = mb.flat_id
+GROUP BY a.apartment_name
+ORDER BY total_revenue DESC;
